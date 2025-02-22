3d667d67ac8991a62bc1890eb54e892cd7d328e0
martinvega2003 <josemartinvt2003@gmail.com>
2025-02-22 15:09:07 -0300

    isAdmin middleware created and added to tasks routes

---
+++ server/src/middlewares/isAdminMiddleware.js
@@ -0,0 +1,15 @@
+export const isAdminMiddleware = async (req, res, next) => {
+    try {
+      const user = await pool.query("SELECT role FROM users WHERE id = $1", [req.user.id]);
+  
+      if (user.rows.length === 0 || user.rows[0].role !== 'admin') {
+        return res.status(403).json({ msg: 'Access denied. Admins only.' });
+      }
+  
+      next(); // Continue if user is an admin
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  };
+  
--- server/src/routes/task.route.js
+++ server/src/routes/task.route.js
@@ -1,25 +1,26 @@
 import { Router } from 'express';
 import { createTask, getAllTasks, getTaskById, updateTask, deleteTask, updateTaskStatus } from '../controllers/task.controller.js';
 import authMiddleware from '../middlewares/authMiddleware.js';
+import { isAdminMiddleware } from '../middlewares/isAdminMiddleware.js';
 
 const router = Router();
 
 // Create a new task
-router.post('/', authMiddleware, createTask);
+router.post('/', authMiddleware, isAdminMiddleware, createTask);
 
 // Get all tasks
-router.get('/', authMiddleware, getAllTasks);
+router.get('/', authMiddleware, isAdminMiddleware, getAllTasks);
 
 // Get task by ID
-router.get('/:id', authMiddleware, getTaskById);
+router.get('/:id', authMiddleware, isAdminMiddleware, getTaskById);
 
 // Update task
-router.put('/:id', authMiddleware, updateTask);
+router.put('/:id', authMiddleware, isAdminMiddleware, updateTask);
 
 // Delete task
-router.delete('/:id', authMiddleware, deleteTask);
+router.delete('/:id', authMiddleware, isAdminMiddleware, deleteTask);
 
 // Route for updating task status
-router.patch('/:id/status', authMiddleware, updateTaskStatus);
+router.patch('/:id/status', authMiddleware, isAdminMiddleware, updateTaskStatus);
 
 export default router;
