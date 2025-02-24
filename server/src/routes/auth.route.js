02778827bc15537e82237570f9984d0da5ff0af0
martinvega2003 <josemartinvt2003@gmail.com>
2025-02-24 10:58:04 -0300

    controller & endpoint for admin users to register new team members (Non-admin) accounts

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,5 +1,7 @@
 import { Router } from 'express';
-import { registerUser, loginUser } from '../controllers/auth.controller.js';
+import { registerUser, loginUser, registerTeamMember } from '../controllers/auth.controller.js';
+import authMiddleware from '../middlewares/authMiddleware.js';
+import { isAdminMiddleware } from '../middlewares/isAdminMiddleware.js';
 
 const router = Router();
 
@@ -8,5 +10,8 @@ router.post('/register/', registerUser);
 
 // Login user
 router.post('/login', loginUser);
+
+// Invite a new team member (Admin only)
+router.post('/invite', authMiddleware, isAdminMiddleware, registerTeamMember);
 
 export default router;
