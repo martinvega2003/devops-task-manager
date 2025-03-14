6b9693f1ca101ffc8f797e362a481bbe6d2d0523
martinvega2003 <josemartinvt2003@gmail.com>
2025-03-14 11:03:12 -0300

    Backend was Dockerized, added Dockerfile and .dockerignore files

--- server/src/server.js
+++ server/src/server.js
@@ -6,6 +6,6 @@ dotenv.config();
 // Start server
 const PORT = process.env.PORT || 5002;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => { //0.0.0.0 allows the app to listen on all network interfaces, not just localhost or 127.0.0.1.
   console.log(`Server started on port ${PORT}`);
 });
