505f88fde1905fdd45b3e32043ab4f3e5518b812
martinvega2003 <josemartinvt2003@gmail.com>
2025-03-17 10:05:14 -0300

    Created the theme mode context provider to toggle between dark and light mode and tested it in the App.jsx file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
-import "./index.css"
-import "./theme.css"
+// App.jsx
+import React, { useContext } from 'react';
+import { ThemeModeProvider, ThemeModeContext } from './context/themeModeContext.jsx';
+import "./theme.css";
 
-const App = () => {
+const Content = () => {
+  const { darkMode, setDarkMode } = useContext(ThemeModeContext);
+
   return (
-    <h2 className='text-heading text-primary'>App</h2>
-  )
-}
+    <div className="app-section min-h-screen app-section">
+      <div className="container mx-auto px-4">
+        <div className="text-center py-16">
+          <h1 className="text-heading text-primary font-bold mb-4">
+            Welcome to Project Manager
+          </h1>
+          <h4 className="text-subheading text-surface-black mb-8">
+            This is a sample page to test my Tailwind CSS configuration with a custom color palette, typography, and layout.
+            <br />
+            It also test the Dark & Light mode Context Provider, all in the App.jsx file.
+          </h4>
+          <button onClick={() => setDarkMode(!darkMode)} className="border-2 border-primary text-white hover:text-primary bg-primary hover:bg-transparent transition duration-300 px-4 py-2 rounded cursor-pointer">
+            Toggle {darkMode ? 'Light' : 'Dark'} Mode
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default App
+const App = () => (
+  <ThemeModeProvider>
+    <Content />
+  </ThemeModeProvider>
+);
+
+export default App;
+
 
