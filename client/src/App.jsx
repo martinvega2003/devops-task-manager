311c488b0ece560bd666b5e3c5625c409d180848
martinvega2003 <josemartinvt2003@gmail.com>
2025-03-21 12:37:19 -0300

    App.jsx was made a functional component. HomePage.jsx was added in the Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { ThemeModeProvider } from './context/themeModeContext.jsx';
 import { AuthProvider } from './context/authContext.jsx';
@@ -6,21 +6,27 @@ import LoginPage from './pages/admin/LoginPage.jsx';
 import Navbar from './components/Navbar.jsx';
 import ProtectedRoute from './components/ProtectedRoute.jsx';
 
-const App = () => (
-  <AuthProvider>
-    <ThemeModeProvider>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path='/login' element={<LoginPage />} />
-          <Route path='/home' element={<ProtectedRoute />}>
-            <Route index element={<h2>Home Page</h2>} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </ThemeModeProvider>
-  </AuthProvider>
-);
+import { AuthContext } from './context/authContext.jsx';
+import HomePage from './pages/HomePage.jsx';
+
+const App = () => {
+
+  return (
+    <AuthProvider>
+      <ThemeModeProvider>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path='/login' element={<LoginPage />} />
+            <Route path='/home' element={<ProtectedRoute />}>
+              <Route index element={<HomePage />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </ThemeModeProvider>
+    </AuthProvider>
+  )
+};
 
 export default App;
 
