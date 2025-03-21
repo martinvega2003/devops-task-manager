07207db1fed6e409f4a55eb0860bb584d568ab16
martinvega2003 <josemartinvt2003@gmail.com>
2025-03-21 10:27:56 -0300

    Button component now accepts and executes onClick function as prop

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Button = ({children}) => {
+const Button = ({children, onClick}) => {
+
   return (
     <button
-      type="submit"
-      onClick={e => handleSubmit}
+      onClick={onClick}
       className="component-button w-full bg-primary dark:bg-secondary text-white py-2 rounded hover:bg-transparent hover:text-primary dark:hover:text-secondary transition duration-300 border-2 border-primary dark:border-secondary cursor-pointer"
     >
       {children}
