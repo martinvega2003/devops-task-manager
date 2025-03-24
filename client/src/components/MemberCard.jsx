2bbe45a90bef4130563d387f66afea40503ba993
martinvega2003 <josemartinvt2003@gmail.com>
2025-03-24 14:04:57 -0300

    Add different colors for member card according to member role

--- client/src/components/MemberCard.jsx
+++ client/src/components/MemberCard.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 
-const MemberCard = ({ children, onClick, className = "" }) => {
+const MemberCard = ({ onClick, className = "", member }) => {
+  
+  let bg = 'bg-white';
+  switch(member.role) {
+    case 'developer':
+      bg = 'bg-blue-500'
+      break;
+    case 'designer':
+      bg = 'bg-yellow-500'
+      break;
+    case 'administrative':
+      bg = 'bg-green-500'
+      break;
+    case 'manager':
+      bg = 'bg-red-500'
+      break;
+  }
+
   return (
     <div
       onClick={onClick}
-      className={`member-card cursor-pointer p-4 rounded shadow-md border border-gray-200 hover:shadow-lg transition duration-300 ${className}`}
+      className={`${bg} cursor-pointer p-4 rounded shadow-md hover:scale-110 transition duration-300 ${className}`}
     >
-      {children}
+      <h3 className="text-body font-semibold">{member.username}</h3>
     </div>
   );
 };
