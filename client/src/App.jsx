c43d2b62dc22e6b6eb6a1c079ad5f1bf249de457
martinvega2003 <josemartinvt2003@gmail.com>
2025-03-24 10:40:10 -0300

    Fix: Take out contextAPI related imports in App.jsx (Not used)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,10 @@
-import React, {useContext} from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { ThemeModeProvider } from './context/themeModeContext.jsx';
 import { AuthProvider } from './context/authContext.jsx';
 import LoginPage from './pages/admin/LoginPage.jsx';
 import Navbar from './components/Navbar.jsx';
 import ProtectedRoute from './components/ProtectedRoute.jsx';
-
-import { AuthContext } from './context/authContext.jsx';
 import HomePage from './pages/HomePage.jsx';
 
 const App = () => {
