7a6042609ec841b12b1f60a9a01d283050cba30c
martinvega2003 <josemartinvt2003@gmail.com>
2025-03-31 12:39:03 -0300

    Modify TaskTitleCard component to receive an style value (Will be needed in Daily tasks modal)

--- client/src/components/TaskTitleCard.jsx
+++ client/src/components/TaskTitleCard.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 
-const TaskTitleCard = ({task, className}) => {
+const TaskTitleCard = ({task, className, style}) => {
   return (
-    <div className={
-      `text-center text-caption text-surface-white py-1 px-3 rounded-md
-      ${task.priority === 'High' ? 'bg-gradient-to-r from-red-700 to-red-500' :
-        task.priority === 'Medium' ? 'bg-gradient-to-r from-yellow-700 to-yellow-500' :
-        'bg-gradient-to-r from-blue-700 to-blue-500'
-      } hover:-translate-y-1 transition duration-200 cursor-pointer ` + className
-    }>
+    <div 
+      className={
+        `text-center text-caption text-surface-white py-1 px-3 rounded-md
+        ${task.priority === 'High' ? 'bg-gradient-to-r from-red-700 to-red-500' :
+          task.priority === 'Medium' ? 'bg-gradient-to-r from-yellow-700 to-yellow-500' :
+          'bg-gradient-to-r from-blue-700 to-blue-500'
+        } hover:-translate-y-1 transition duration-200 cursor-pointer ` + className
+      }
+      style={style && style}
+    >
       {task.title}
     </div>
   )
