7e47e6b1805abfa2ad729f448c99b3befe3c89cf
martinvega2003 <josemartinvt2003@gmail.com>
2025-05-28 12:52:19 -0300

    Update time block style and display of value

--- client/src/pages/admin/TaskPage.jsx
+++ client/src/pages/admin/TaskPage.jsx
@@ -371,14 +371,14 @@ const TaskPage = ({ selectedTask, setSelectedTask, fetchTasks }) => {
 
               {/* Task Duration */}
               <div className="min-w-full flex flex-col gap-2">
-                <div className={`min-w-full flex ${!isEditing ? '': 'flex-col lg:flex-row'} items-start lg:items-center gap-4 p-4 border border-gray-300 dark:border-gray-600 rounded-2xl bg-white dark:bg-gray-700`}>
+                <div className={`min-w-full flex ${!isEditing ? 'items-center': 'flex-col lg:flex-row items-start lg:items-center'} gap-2 p-4 border border-gray-300 dark:border-gray-600 rounded-2xl bg-white dark:bg-gray-700`}>
                   <div className="flex flex-col items-start">
                     <span className="text-caption font-semibold text-gray-600 dark:text-gray-400 whitespace-nowrap">
                       Start Time
                     </span>
                     {!isEditing ? (
                       <span className="text-body font-medium text-surface-black dark:text-surface-white">
-                        {updatedTask.startTime}
+                        {String(new Date(selectedTask.start_time).getHours()).padStart(2, '0')}:{String(new Date(selectedTask.start_time).getMinutes()).padStart(2, '0')}
                       </span>
                     ) : (
                       <div className="flex items-center gap-4">
@@ -430,14 +430,14 @@ const TaskPage = ({ selectedTask, setSelectedTask, fetchTasks }) => {
                       </div>
                     )}
                   </div>
-                  <div className="w-full mx-1 border border-dashed" />
+                  <div className="w-full mx-1 border-2 border-gray-400 dark:border-gray-500 rounded-lg" />
                   <div className="flex flex-col items-start">
                     <span className="text-caption font-semibold text-gray-600 dark:text-gray-400 whitespace-nowrap">
                       End Time
                     </span>
                     {!isEditing ? (
                       <span className="text-body font-medium text-surface-black dark:text-surface-white">
-                        {updatedTask.endTime}
+                        {String(new Date(selectedTask.end_time).getHours()).padStart(2, '0')}:{String(new Date(selectedTask.end_time).getMinutes()).padStart(2, '0')}
                       </span>
                     ) : (
                       <div className="flex items-center gap-4">
